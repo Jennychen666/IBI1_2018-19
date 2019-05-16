40974a263e96941363cb88797098c1d41aff5696
Jenny <Jiajia.18@intl.zju.edu.cn>
2019-05-16 23:06:49 +0800

    Updated Practical5

--- Practical5/countingDNA.py
+++ Practical5/countingDNA.py
@@ -4,21 +4,24 @@ Created on Wed Mar 20 08:53:10 2019
 
 @author: admin
 """
-
+#the user should give a DNA string of length
 sequence=input("Please enter a DNA sequence:", )
-#the code was amended to  have a prompt for users and word without spaces
+#count the respective number of times that the symbols 'A', 'C', 'G', and 'T' 
+#Create a frequency dictionary mapping str:int 
 myDirt={}
+#set the loop to search the sequence
 for word in sequence:
     if word in myDirt:
         myDirt[word]+=1
     else:
         myDirt[word]=1
+#show the numebers of four nucleotides
 print(myDirt)
-
+#construct a pie from the data
 import matplotlib.pyplot as plt
 labels='A','T','G','C'
-sizes=tuple(myDirt.values())
-explode=(0.1,0,0,0)
+sizes=tuple(myDirt.values()) 
+explode=(0.1,0,0,0)   #the part of 'A' will be seperated from the pie chart a bit
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',
         shadow=True,startangle=90)
--- Practical5/string reverse and list sorting.py
+++ Practical5/string reverse and list sorting.py
@@ -4,9 +4,10 @@ Created on Wed Mar 20 10:03:34 2019
 
 @author: admin
 """
-
-sequence=str(input('Please input a string:',))
 #improvements:ask the user for an input string, instead of hard-coding the string in the script.
+sequence=str(input('Please input a string:',))
+#reverse the order
+#use split() to get words from a string
 sequence=sequence[ ::-1]
 sequence=sequence.split(" ")
 
